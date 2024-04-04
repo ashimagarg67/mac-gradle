ffbb9e1abd3291aee5fd003d428b07f5a815414e
ashimagarg67 <ashima.garg67@gmail.com>
2024-04-04 12:46:11 +0530

    Add files via upload

---
+++ src/main/java/com/example/macgradle01/MacGradle01Application.java
@@ -0,0 +1,13 @@
+package com.example.macgradle01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MacGradle01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MacGradle01Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/macgradle01/MacGradle01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.macgradle01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MacGradle01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
